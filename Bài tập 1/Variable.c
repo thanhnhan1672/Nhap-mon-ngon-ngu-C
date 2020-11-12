7d84c34d84dfda2cbea9fba7d03e41c025a72db7
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2020-11-11 21:01:30 -0700

    Create Variable.c

---
+++ Bài tập 1/Variable.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+	// khai bao bien num kieu int 
+	int num;
+	// khai bao gia tri bien num
+	num = 5;
+	//hien thi bien num ra mang hinh  
+	printf("Bien num co gia tri: %d \n",num) ; 
+	// khai bao bien x kieu char
+	char c;
+	// khoi tao gia tri cho bien c
+	c = 'a';
+	// hien thi bien c ra man hinh
+	printf (" bien c co gia tri: %c \n", c); 
+	// hien thi  dong: bien num =5 va bien c = a 
+	printf ("bien num = %d va bien c = %c \n", num ,c  );
+	getchar();  
+return 0; 
+ 
+} 
