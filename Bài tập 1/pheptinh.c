57b6c1e5e840e0b481f4e35542499b21524ecfd0
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2020-11-25 18:27:33 -0700

    pheptinh.c

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () {
+
+int a = 3 ;
+int b = 5  ;
+    printf("a + b = %d,", a + b );
+	printf("a - b = %d,", a - b);
+	printf("a * b = %d,", a * b);
+	printf("a / b = %d,", a / b);
+	double c = 3;
+	double d = 5;
+	printf ("c / d =%f ",c / d); 
+	return 0; 
+}
