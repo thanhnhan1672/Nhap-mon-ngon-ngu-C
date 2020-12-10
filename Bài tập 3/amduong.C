6f1962b969dec642497d41a692a1b5ca27c2dc00
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2020-12-09 21:04:51 -0500

    Update amduong.C

--- Bài tập 3/amduong.C
+++ Bài tập 3/amduong.C
@@ -10,10 +10,8 @@ int main(){
 	// khai bao bien a > 0 la so duong
 if(a > 0) {
 	printf("%d la so duong",a);
-	//khai bao bien a = 0 la bang 0
-if(a = 0) {
-		printf("%d bang khong", a);
-	}
+	
+
 	// neu a bang 0 thi bang khong
 } else if(a == 0) {
 
