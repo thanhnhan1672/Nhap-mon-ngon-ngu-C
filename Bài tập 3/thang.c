1b71b054ba5a2f3e99b9c1f578526636c305de04
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2020-12-17 11:01:15 +0700

    Create thang.c

---
+++ Bài tập 3/thang.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+	int thang;
+	printf(" Nhap thang :");
+	scanf("%d",&thang);
+	switch(thang){
+		case 1:
+			printf(" 31 ngay"); break;
+		case 2:
+			printf(" 28 ngay"); break;
+		case 3:
+			printf(" 31 ngay"); break;
+		case 4:
+			printf(" 30 ngay"); break;
+		case 5:
+			printf(" 31 ngay"); break;
+		case 6:
+			printf(" 30 ngay"); break;
+		case 7:
+			printf(" 31 ngay"); break;
+		case 8:
+			printf(" 31 ngay"); break;
+		case 9:
+			printf(" 30 ngay"); break;
+		case 10:
+			printf(" 31 ngay"); break;	
+		case 11:
+			printf(" 30 ngay"); break;
+		case 12:
+			printf(" 31 ngay"); break;
+	}
+		 return 0;
+}
