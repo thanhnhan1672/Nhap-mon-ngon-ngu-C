1e236200e135b651361669565adc6cc81f5ede19
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2020-12-24 11:04:37 +0700

    Create tong10.c

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+		int dem;
+	int tong  ;
+	 tong = 0 ;
+	 int n;
+	 printf(" nhap gia tri n:",n);
+	 scanf("%d",&n); 
+	 for (dem = 1; dem <= n ;dem++){
+	 	tong = tong + dem;
+	 }
+	 printf ("tong la : %d", tong);
+	return 0;
+	
+}
