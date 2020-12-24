d1fb067735ab4ae77a55104ef23cb5d817c842b3
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2020-12-24 08:57:11 +0700

    Update motdenmuoi.c

--- Bài tập 3/motdenmuoi.c
+++ Bài tập 3/motdenmuoi.c
@@ -5,6 +5,8 @@ main(){
   for(a=1; a<11; a++){ // vong lap 1 den 10 
     printf("%d ",a);  // xuat ket qua cach nhau 1 khoang trang
   }
+for(a=1; a<11; a++){ // vong lap 1 den 10 
+    printf("\n%d",a); // xuat hien ket qua ra 10 hang
 
   return 0;
 }
