02797fc95c302beaa52d28e1573ddb67d5d2d91d
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-07 10:25:54 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -9,3 +9,24 @@ int main () {
     }
     return 0;
 }
+// khong dung for
+#include<stdio.h>
+int main () {
+	
+	printf("Bang cuu chuong");
+	int n;
+	n = 2;
+	printf("bang 2\n");
+	printf(" 2*1 =%d\n 2*2 = %d\n 2*3 =%d\n",n*1,n*2,n*3);
+	n = 3;
+	printf("bang 3\n");
+	printf(" 3*1 =%d\n 3*2 = %d\n 3*3 =%d\n",n*1,n*2,n*3);
+	n = 4;
+	printf("bang 4\n");
+	printf(" 4*1 =%d\n 4*2 = %d\n 4*3 =%d\n",n*1,n*2,n*3);
+	n = 5;
+	printf("bang 5\n");
+	printf(" 5*1 =%d\n 5*2 = %d\n 5*3 =%d\n",n*1,n*2,n*3);
+	
+	return 0;
+	}
