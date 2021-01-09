dc15c65988dc27fc8bb54d0c58c23918acd88a41
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-09 11:10:24 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -2,12 +2,20 @@
 
 int main ()
 {
+int h = 4;
+
 // h = 4 4:4 hang
 // hang 1 : 1 sao 3 khoang trang
 // hang 2 : 3 sao 2 khoang trang
 // hang 3 : 5 sang 1 khoang trang
 // hang 4 : 7 sao 0 khoang trang
-
+for(int i = 1; i <= h ;i++){
+  for(int j = 1; j <=10 ; j++)
+printf(" ");
+for(int k = 1; k <=2 * i - 1;k++)
+printf("*");
+printf("\n");
+}
 
 // h = 3 3:3 hang
 // hang 1 :1 sao 2 khoang trang
