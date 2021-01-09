72c75827bc6adf2d84f73f673c946a7ce502fa1e
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-09 10:19:40 +0700

    Create kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main ()
+{
+// h = 4 4:4 hang
+// hang 1 : 1 sao 3 khoang trang
+// hang 2 : 3 sao 2 khoang trang
+// hang 3 : 5 sang 1 khoang trang
+// hang 4 : 7 sao 0 khoang trang
+
+
+// h = 3 3:3 hang
+// hang 1 :1 sao 2 khoang trang
+// hang 2 : 3 sao 1khoang trang
+// hang 1 : 5 sao 0 khoang trang
+
+
+// h = 2 2:2 hang
+// hang 1: 1 sao 2 khoang trang
+// hang 2 : 3 sao khong khoang trang
+
+//h = 1 1:1 hang
+// hang 1 : 1 sao
+
+
+    return 0;
+}
