719c8c7410eae5a79a34ec746342a39c88169256
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-09 09:59:33 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -11,3 +11,19 @@
         printf("tong cac chu so cua n la: %d", S);
     return 0;
  }
+
+
+#include<stdio.h>
+ int main() {
+ 	int n = 123;
+ 	int s = 0;
+ 	int so ;
+ 	for(int i = 1; n > 0; i++)}
+ 		so = n % 10;
+ 		n = n / 10;
+ 		s = s + so;
+ 		{
+ 		printf("%d",s);
+	 
+ 	return 0;
+ 	}
