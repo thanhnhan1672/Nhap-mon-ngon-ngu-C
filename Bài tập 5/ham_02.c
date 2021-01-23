fe84bc6579763d24dad6fe2b9574253fd2192ccc
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-23 16:28:56 +0700

    Create ham_02.c

---
+++ Bài tập 5/ham_02.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+
+int kiem_tra_am_duong(int n){}
+
+int main(){
+int n;
+printf("Nhap n = ");
+scanf("%d", &n);
+if(kiem_tra_am_duong(n) > 0)
+printf("n la so nguyen duong ");
+else if (kiem_tra_am_duong(n) < 0)
+printf("n la so nguyen âm");
+else
+printf("n = 0");
+return 0;
+}
