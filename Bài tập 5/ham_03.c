7986aafdc65d486478f8bae1b9797e305fbae1ad
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-28 10:57:22 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+void Nhap_pt_mang(int n, int a[]){
 
+}
+
+void Hien_thi_pt_mang(int n, int a[]){
+
+}
+
+void Hien_thi_tong_chan(int n, int a[]){
+
+}
+
+void Dem_chan_le_0(int n, int a[]){
+
+}
+    
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
