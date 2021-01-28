1e61def45661154c6343f9e1368304e9396a9a54
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-28 15:48:37 +0700

    Update TH_02.c

--- Bài tập 5/TH_02.c
+++ Bài tập 5/TH_02.c
@@ -2,12 +2,15 @@
 int main(){
 		int S = 0;
 	 int n;
-	 printf(" nhap gia tri n:",n);
+	 printf(" nhap gia tri n:");
 	 scanf("%d",&n); 
+	 printf("S=");
 	 for (int i = 1; i <= n ;i++){
+     
 	 	S = S + i;
+	 printf (" +%d", i);
 	 }
-	 printf ("tong la : %d", S);
+	 printf (" = %d", S);
 	return 0;
 	
 }
