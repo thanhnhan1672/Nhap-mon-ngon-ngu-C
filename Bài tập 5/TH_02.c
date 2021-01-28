5391355064213bc1d3de243ea7798a11c22114bd
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-28 15:37:42 +0700

    Create TH_02.c

---
+++ Bài tập 5/TH_02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+		int S = 0;
+	 int n;
+	 printf(" nhap gia tri n:",n);
+	 scanf("%d",&n); 
+	 for (int i = 1; i <= n ;i++){
+	 	S = S + i;
+	 }
+	 printf ("tong la : %d", S);
+	return 0;
+	
+}
