9bbb10fd70b52f88f0d0183a1be8c42934977304
thanhnhan1672 <72533372+thanhnhan1672@users.noreply.github.com>
2021-01-28 13:45:20 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,28 +1,24 @@
 #include<stdio.h>
-void Nhap_pt_mang(int n, int a[]){
+void Nhap_pt_mang(int n, int a[]);
 
-}
+void Hien_thi_pt_mang(int n, int a[]);
 
-void Hien_thi_pt_mang(int n, int a[]){
+void Hien_thi_tong_chan(int n, int a[]);
 
-}
-
-void Hien_thi_tong_chan(int n, int a[]){
-
-}
-
-void Dem_chan_le_0(int n, int a[]){
-
-}
+void Dem_chan_le_0(int n, int a[]);
     
 int main(){
   int n;
   printf("Nhap n = ");
   scanf("%d", &n);
   int a[n];
-  Nhap_pt_mang(a);
-  Hien_thi_pt_mang(a);
-  Hien_thi_tong_chan(a);
-  Dem_chan_le_0(a);
+  Nhap_pt_mang(n, a);
+  Hien_thi_pt_mang(n, a);
+  Hien_thi_tong_chan(n, a);
+  Dem_chan_le_0(n, a);
   return 0;
 }
+void Nhap_pt_mang(int n, int a[]){}
+void Hien_thi_pt_mang(int n, int a[]){}
+void Hien_thi_tong_chan(int n, int a[]){}
+void Dem_chan_le_0(int n, int a[]){}
